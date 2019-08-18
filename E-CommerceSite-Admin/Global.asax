a08e4c161acde7cd4addb8b15cad188120efeadd
Prabhash <prabhashkmr007@gmail.com>
2019-08-18 14:11:46 +0530

    init

---
+++ E-CommerceSite-Admin/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="E_CommerceSite_Admin.MvcApplication" Language="C#" %>
---
+++ E-CommerceSite-Admin/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(E_CommerceSite_Admin.Startup))]
+namespace E_CommerceSite_Admin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
