72c68c2856aa526acf9768438711b86b60bb4dad
Prabhash <prabhashkmr007@gmail.com>
2019-08-22 01:52:09 +0530

    Database Connection Complete

---
+++ E-CommerceSite.Bussiness/DAL/SignUpHelper.cs
@@ -0,0 +1,29 @@
+﻿using E_CommerceSite.DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_CommerceSite.Bussiness.DAL
+{
+   public class SignUpHelper
+    {
+        public List<SignUpViewModels> ListUser()
+        {
+            List<SignUpViewModels> data = new List<SignUpViewModels>();
+            try
+            {
+                using (ECommerceDBContext db = new ECommerceDBContext())
+                {
+                    data = db.SignUpViewModel.ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return data;
+        }
+    }
+}
